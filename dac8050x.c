79b97a95929146f7bc6130161f46b1222601a260
LinceTachlog <lincechacko99@gmail.com>
2025-07-23 13:15:05 +0530

    added a basic untested complet driver for the DAC

--- dac8050x.c
+++ dac8050x.c
@@ -58,7 +58,7 @@ DAC8050x_CONFIG dac8050x_config =
   * @param  None
   * @retval None
   */
-/*change this function according to your controller function*/
+#warning change this function according to your controller function
 void enable_chipSelect(void)
 {
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
@@ -69,7 +69,7 @@ void enable_chipSelect(void)
   * @param  None
   * @retval None
   */
-/*change this function according to your controller function*/
+#warning change this function according to your controller function
 void disable_chipSelect(void)
 {
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
@@ -80,11 +80,13 @@ void disable_chipSelect(void)
   * @param  data : data to be transmitted
   * @retval return the transmission is success or not
   */
-/*change this function according to your controller function*/
+#warning change this function according to your controller function
 bool dac8050x_transmitData(uint8_t * data)
 {
-	  HAL_SPI_Transmit(&hspi1, data, 3, 1000);
-	  return true;
+	HAL_StatusTypeDef status = HAL_OK;
+	HAL_SPI_Transmit(&hspi1, data, NUM_THREE, 1000);
+	if(status == HAL_OK) return (true);
+	else return (false);
 
 }
 
@@ -93,36 +95,202 @@ bool dac8050x_transmitData(uint8_t * data)
   * @param  data : data to be transmitted
   * @retval return the transmission is success or not
   */
-/*change this function according to your controller function*/
+#warning change this function according to your controller function
 bool dac8050x_receiveData(uint8_t * data)
 {
-	  HAL_SPI_Receive(&hspi1, data, 3, 1000);
-	  return true;
+	HAL_StatusTypeDef status = HAL_OK;
+	status = HAL_SPI_Receive(&hspi1, data, NUM_THREE, 1000);
+	if(status == HAL_OK) return (true);
+	else return (false);
+
+}
+
+
+/**
+  * @brief  function to write to the config register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeSyncRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = WRITE_REGISTER_COMMAND | REGISTER_DAC_SYNC;
+	tempBufferTransmit[NUM_ONE] =  ((uint16_t)CONFIG_SYNC_REGISTER_VALUE >> 8);
+	tempBufferTransmit[NUM_TWO] =  ((uint8_t)CONFIG_SYNC_REGISTER_VALUE);
+	return (dac8050x_writeRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to write to the config register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeConfigRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = WRITE_REGISTER_COMMAND | REGISTER_DAC_CONFIG;
+	tempBufferTransmit[NUM_ONE] =  ((uint16_t)CONFIG_CONFIG_REGISTER_VALUE >> 8);
+	tempBufferTransmit[NUM_TWO] =  ((uint8_t)CONFIG_CONFIG_REGISTER_VALUE);
+	return (dac8050x_writeRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to write to the gain register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeGainRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = WRITE_REGISTER_COMMAND | REGISTER_DAC_GAIN;
+	tempBufferTransmit[NUM_ONE] =  ((uint16_t)CONFIG_GAIN_REGISTER_VALUE >> 8);
+	tempBufferTransmit[NUM_TWO] =  ((uint8_t)CONFIG_GAIN_REGISTER_VALUE);
+	return (dac8050x_writeRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to write to the trigger register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeTriggerRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = WRITE_REGISTER_COMMAND | REGISTER_DAC_TRIGGGER;
+	tempBufferTransmit[NUM_ONE] =  ((uint16_t)CONFIG_TRIGGER_REGISTER_VALUE >> 8);
+	tempBufferTransmit[NUM_TWO] =  ((uint8_t)CONFIG_TRIGGER_REGISTER_VALUE);
+	return (dac8050x_writeRegister(tempBufferTransmit));
 }
 
 /**
   * @brief  function to read device ID
   * @param  None
-  * @retval return the 16 bit data from the corresponding register
+  * @retval return the 16 bit device ID.
   */
 /*function will return both device ID and version ID first two bit is version ID (LSB)*/
-uint16_t readDeviceID(void)
+uint16_t dac8050x_readDeviceID(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_DEVICE_ID;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read sync register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readSyncRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_SYNC;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read configuration register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readConfigRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_CONFIG;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read Gain register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readGainRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_GAIN;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read broadcast register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readBroadcastRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_BROADCAST;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read status register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readStatusRegister(void)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_STATUS;
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read DAC channel value
+  * @param  channelNo : DAC channel number to get the value
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readDacRegValue(uint8_t channelNo)
+{
+	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
+	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | (REGISTER_DAC_CHANNEL_0 + channelNo);
+	return (dac8050x_readRegister(tempBufferTransmit));
+}
+
+/**
+  * @brief  function to read the register
+  * @param  None
+  * @retval return the 16 bit data from the corresponding register
+  */
+/*the input parameter should contain the read command + address of the register*/
+uint16_t dac8050x_readRegister(uint8_t * data)
 {
 	bool status = false;
-	uint8_t tempBufferTransmit[NUM_THREE] = {INIT_ZERO};
 	uint8_t tempBufferReceive[NUM_THREE] = {INIT_ZERO};
-	tempBufferTransmit[NUM_ZERO] = READ_REGISTER_COMMAND | REGISTER_DAC_DEVICE_ID;
 	dac8050x_config.enable_chipSelect();
-	status = dac8050x_config.spi_transferData(tempBufferTransmit);
+	HAL_Delay(1);
+	status = dac8050x_config.spi_transferData(data);
 	if(!status)
 	{
 		return(NUM_ZERO);
 	}
 	status = dac8050x_config.spi_ReceiveData(tempBufferReceive);
+	HAL_Delay(1);
 	dac8050x_config.disable_chipSelect();
 	if(!status)
 	{
 		return(NUM_ZERO);
 	}
-	return ((tempBufferReceive[NUM_TWO] << 8) | tempBufferReceive[NUM_THREE] ); /*device ID + version id*/
+	return ((tempBufferReceive[NUM_ONE] << 8) | tempBufferReceive[NUM_TWO] ); /*device ID + version id*/
+}
+
+/**
+  * @brief  function to write the register
+  * @param  None
+  * @retval return the 16 bit data from the corresponding register
+  */
+/*the input parameter should contain the write command + address of the register + the data need to be written to the register*/
+bool dac8050x_writeRegister(uint8_t * data)
+{
+	bool status = false;
+	dac8050x_config.enable_chipSelect();
+	HAL_Delay(1);
+	status = dac8050x_config.spi_transferData(data);
+	HAL_Delay(1);
+	dac8050x_config.disable_chipSelect();
+	if(!status)
+	{
+		return(NUM_ZERO);
+	}
+	return true;
 }
--- dac8050x.h
+++ dac8050x.h
@@ -74,16 +74,160 @@
 #define REGISTER_DAC_CHANNEL_6 	                           0x0E /*register address for the DAC channel 6 - DAC80502*/
 #define REGISTER_DAC_CHANNEL_7 	                           0x0F /*register address for the DAC channel 7 - DAC80502*/
 
-
 #define READ_REGISTER_COMMAND                              0x80  /*command for reading register*/
 #define WRITE_REGISTER_COMMAND                             0x00  /*command for writing register*/
 
-#define DAC7_BROADCAST_ENABLE                              1<<15;
-#define DAC7_BROADCAST_DISABLE                             0<<15;
+/*--------------------------------------SYNCHRONOUS ENABLE / DISABLE---------------------------------------------------*/
+#define DAC7_SYNC_ENABLE                                   1<<7 /*bit to enable the SYNC for the DAC channel 7*/
+#define DAC7_SYNC_DISABLE                                  0<<7 /*bit to disable the SYNC for the DAC channel 7*/
+
+#define DAC6_SYNC_ENABLE                                   1<<6 /*bit to enable the SYNC for the DAC channel 6*/
+#define DAC6_SYNC_DISABLE                                  0<<6 /*bit to disable the SYNC for the DAC channel 6*/
+
+#define DAC5_SYNC_ENABLE                                   1<<5 /*bit to enable the SYNC for the DAC channel 5*/
+#define DAC5_SYNC_DISABLE                                  0<<5 /*bit to disable the SYNC for the DAC channel 5 */
+
+#define DAC4_SYNC_ENABLE                                   1<<4 /*bit to enable the SYNC for the DAC channel 4*/
+#define DAC4_SYNC_DISABLE                                  0<<4 /*bit to disable the SYNC for the DAC channel 4 */
+
+#define DAC3_SYNC_ENABLE                                   1<<3 /*bit to enable the SYNC for the DAC channel 3*/
+#define DAC3_SYNC_DISABLE                                  0<<3 /*bit to disable the SYNC for the DAC channel 3*/
+
+#define DAC2_SYNC_ENABLE                                   1<<2 /*bit to enable the SYNC for the DAC channel 2*/
+#define DAC2_SYNC_DISABLE                                  0<<2 /*bit to disable the SYNC for the DAC channel 2*/
+
+#define DAC1_SYNC_ENABLE                                   1<<1 /*bit to enable the SYNC for the DAC channel 1*/
+#define DAC1_SYNC_DISABLE                                  0<<1 /*bit to disable the SYNC for the DAC channel 1 */
+
+#define DAC0_SYNC_ENABLE                                   1<<0 /*bit to enable the SYNC for the DAC channel 0*/
+#define DAC0_SYNC_DISABLE                                  0<<0 /*bit to disable the SYNC for the DAC channel 0 */
+
+/*--------------------------------------BROADCAST ENABLE AND DISABLE----------------------------------------------*/
+#define DAC7_BROADCAST_ENABLE                              1<<15 /*bit to enable the broadcast for the DAC channel 7*/
+#define DAC7_BROADCAST_DISABLE                             0<<15 /*bit to disable the broadcast for the DAC channel 7*/
+
+#define DAC6_BROADCAST_ENABLE                              1<<14 /*bit to enable the broadcast for the DAC channel 6*/
+#define DAC6_BROADCAST_DISABLE                             0<<14 /*bit to disable the broadcast for the DAC channel 6*/
+
+#define DAC5_BROADCAST_ENABLE                              1<<13 /*bit to enable the broadcast for the DAC channel 5*/
+#define DAC5_BROADCAST_DISABLE                             0<<13 /*bit to disable the broadcast for the DAC channel 5 */
+
+#define DAC4_BROADCAST_ENABLE                              1<<12 /*bit to enable the broadcast for the DAC channel 4*/
+#define DAC4_BROADCAST_DISABLE                             0<<12 /*bit to disable the broadcast for the DAC channel 4 */
+
+#define DAC3_BROADCAST_ENABLE                              1<<11 /*bit to enable the broadcast for the DAC channel 3*/
+#define DAC3_BROADCAST_DISABLE                             0<<11 /*bit to disable the broadcast for the DAC channel 3*/
+
+#define DAC2_BROADCAST_ENABLE                              1<<10 /*bit to enable the broadcast for the DAC channel 2*/
+#define DAC2_BROADCAST_DISABLE                             0<<10 /*bit to disable the broadcast for the DAC channel 2*/
+
+#define DAC1_BROADCAST_ENABLE                              1<<9 /*bit to enable the broadcast for the DAC channel 1*/
+#define DAC1_BROADCAST_DISABLE                             0<<9 /*bit to disable the broadcast for the DAC channel 1 */
+
+#define DAC0_BROADCAST_ENABLE                              1<<8 /*bit to enable the broadcast for the DAC channel 0*/
+#define DAC0_BROADCAST_DISABLE                             0<<8 /*bit to disable the broadcast for the DAC channel 0 */
+
+/*-------------------------------------------CONFIG REGISTER-------------------------------------------------------*/
+#define ALARM_PIN_CRC_ERROR                                0<<13
+#define ALARM_PIN_REF_ALARM                                1<<13
+
+#define CONFIG_ALARM_ENABLE                                1<<12
+#define CONFIG_ALARM_DISABLE                               0<<12
+
+#define CONFIG_CRC_ENABLE                                  1<<11
+#define CONFIG_CRC_DISABLE                                 0<<11
+
+#define CONFIG_UPDATE_RISING_EDGE                          0<<10
+#define CONFIG_UPDATE_FALLING_EDGE                         1<<10
+
+#define CONFIG_DISABLE_SERIAL_OUT                          1<<9
+#define CONFIG_ENABLE_SERIAL_OUT                           0<<9
+
+#define CONFIG_DISABLE_INTERNAL_REF                        1<<8
+#define CONFIG_ENABLE_INTERNAL_REF                         0<<8
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC7                  1<<7
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC7                 0<<7
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC6                  1<<6
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC6                 0<<6
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC5                  1<<5
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC5                 0<<5
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC4                  1<<4
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC4                 0<<4
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC3                  1<<3
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC3                 0<<3
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC2                  1<<2
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC2                 0<<2
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC1                  1<<1
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC1                 0<<1
+
+#define CONFIG_ENABLE_MODE_POWERDOWN_DAC0                  1<<0
+#define CONFIG_DISABLE_MODE_POWERDOWN_DAC0                 0<<0
+
+/*---------------------------------------------GAIN REGISTER-------------------------------------------------*/
+#define GAIN_REG_DIV_INT_REF_0                             0<<8
+#define GAIN_REG_DIV_INT_REF_2                             1<<8
+
+#define GAIN_DAC_CHANNEL_7_VALUE_2                         1<<7
+#define GAIN_DAC_CHANNEL_7_VALUE_1                         0<<7
+
+#define GAIN_DAC_CHANNEL_6_VALUE_2                         1<<6
+#define GAIN_DAC_CHANNEL_6_VALUE_1                         0<<6
+
+#define GAIN_DAC_CHANNEL_5_VALUE_2                         1<<5
+#define GAIN_DAC_CHANNEL_5_VALUE_1                         0<<5
+
+#define GAIN_DAC_CHANNEL_4_VALUE_2                         1<<4
+#define GAIN_DAC_CHANNEL_4_VALUE_1                         0<<4
+
+#define GAIN_DAC_CHANNEL_3_VALUE_2                         1<<3
+#define GAIN_DAC_CHANNEL_3_VALUE_1                         0<<3
+
+#define GAIN_DAC_CHANNEL_2_VALUE_2                         1<<2
+#define GAIN_DAC_CHANNEL_2_VALUE_1                         0<<2
+
+#define GAIN_DAC_CHANNEL_1_VALUE_2                         1<<1
+#define GAIN_DAC_CHANNEL_1_VALUE_1                         0<<1
+
+#define GAIN_DAC_CHANNEL_0_VALUE_2                         1<<0
+#define GAIN_DAC_CHANNEL_0_VALUE_1                         0<<0
+
+/*------------------------------------------TRIGGER REGISTER-------------------------------------------------*/
+#define TRIGGER_LOAD_SYNCHRONOUS_VALUE_ON                  1<<4
+#define TRIGGER_LOAD_SYNCHRONOUS_VALUE_OFF                 0<<4
+#define SOFT_RESET_DAC_ON                                  0x0A
+#define SOFT_RESET_DAC_OFF                                 0x00
 
 
+/*------------------------------------------DAC CONFIGURATIONS---------------------------------------------------------*/
+#define CONFIG_SYNC_REGISTER_VALUE \
+	                (DAC7_BROADCAST_ENABLE | DAC6_BROADCAST_ENABLE| DAC5_BROADCAST_ENABLE | DAC4_BROADCAST_ENABLE \
+                     |DAC3_BROADCAST_ENABLE | DAC2_BROADCAST_ENABLE | DAC1_BROADCAST_ENABLE| DAC0_BROADCAST_ENABLE \
+                     |DAC7_SYNC_DISABLE | DAC6_SYNC_DISABLE | DAC5_SYNC_DISABLE | DAC4_SYNC_DISABLE| DAC3_SYNC_DISABLE \
+					 | DAC2_SYNC_DISABLE |DAC1_SYNC_DISABLE | DAC0_SYNC_DISABLE)
+
+#define CONFIG_CONFIG_REGISTER_VALUE \
+	                (ALARM_PIN_REF_ALARM | CONFIG_ALARM_DISABLE | CONFIG_UPDATE_RISING_EDGE |CONFIG_ENABLE_SERIAL_OUT \
+	                 |CONFIG_ENABLE_INTERNAL_REF	|CONFIG_DISABLE_MODE_POWERDOWN_DAC7 | CONFIG_DISABLE_MODE_POWERDOWN_DAC6\
+					  |CONFIG_DISABLE_MODE_POWERDOWN_DAC5 | CONFIG_DISABLE_MODE_POWERDOWN_DAC4 | CONFIG_DISABLE_MODE_POWERDOWN_DAC3\
+					  |CONFIG_DISABLE_MODE_POWERDOWN_DAC2 | CONFIG_DISABLE_MODE_POWERDOWN_DAC1 | CONFIG_DISABLE_MODE_POWERDOWN_DAC0)
+
+#define CONFIG_GAIN_REGISTER_VALUE \
+	                (GAIN_REG_DIV_INT_REF_0 | GAIN_DAC_CHANNEL_7_VALUE_2 | GAIN_DAC_CHANNEL_6_VALUE_2 | GAIN_DAC_CHANNEL_5_VALUE_2\
+	                |GAIN_DAC_CHANNEL_4_VALUE_2 | GAIN_DAC_CHANNEL_3_VALUE_2	| GAIN_DAC_CHANNEL_2_VALUE_2 |GAIN_DAC_CHANNEL_1_VALUE_2\
+				   | GAIN_DAC_CHANNEL_0_VALUE_2)
+
+#define CONFIG_TRIGGER_REGISTER_VALUE \
+	                (TRIGGER_LOAD_SYNCHRONOUS_VALUE_OFF | SOFT_RESET_DAC_OFF )
 
 
+/*STRUCTURE FOR THE SPI CONFIG FOR THE DAC*/
 typedef struct
 {
 	bool (* spi_transferData) (uint8_t *data);
@@ -120,11 +264,91 @@ bool dac8050x_transmitData(uint8_t * data);
   */
 bool dac8050x_receiveData(uint8_t * data);
 
+/**
+  * @brief  function to write to the config register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeSyncRegister(void);
 
+/**
+  * @brief  function to write to the config register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeConfigRegister(void);
 
+/**
+  * @brief  function to write to the gain register
+  * @param  None
+  * @retval return the transmission is success or not
+  */
+bool writeGainRegister(void);
 
+/**
+  * @brief  function to read device ID
+  * @param  None
+  * @retval return the 16 bit device ID.
+  */
+/*function will return both device ID and version ID first two bit is version ID (LSB)*/
+uint16_t dac8050x_readDeviceID(void);
 
+/**
+  * @brief  function to read sync register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readSyncRegister(void);
 
+/**
+  * @brief  function to read configuration register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readConfigRegister(void);
+
+/**
+  * @brief  function to read Gain register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readGainRegister(void);
+
+/**
+  * @brief  function to read broadcast register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readBroadcastRegister(void);
+
+/**
+  * @brief  function to read status register
+  * @param  None
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readStatusRegister(void);
+
+/**
+  * @brief  function to read DAC channel value
+  * @param  channelNo : DAC channel number to get the value
+  * @retval return the 16 bit register value
+  */
+uint16_t dac8050x_readDacRegValue(uint8_t channelNo);
+
+/**
+  * @brief  function to read the register
+  * @param  None
+  * @retval return the 16 bit data from the corresponding register
+  */
+uint16_t dac8050x_readRegister(uint8_t * data);
+
+/**
+  * @brief  function to write the register
+  * @param  None
+  * @retval return the 16 bit data from the corresponding register
+  */
+/*the input parameter should contain the write command + address of the register + the data need to be written to the register*/
+bool dac8050x_writeRegister(uint8_t * data);
 
 
 
